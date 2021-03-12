1e5e4de0b729f7bd57e9370e7c91b99cf6036f1b
hstol <hstol@LAPTOP-HBRRB4RJ>
2021-03-11 22:43:59 -0600

    Agrege costructors y overrides en clientes y espleados. y agrege la interfas de operaciones

--- src/main/java/com/upemor/ep1/Entidades/Cliente.java
+++ src/main/java/com/upemor/ep1/Entidades/Cliente.java
@@ -3,7 +3,7 @@ package com.upemor.ep1.Entidades;
 /**
  @author JORGE
  **/
-public class Cliente {
+public class Cliente implements Operaciones{
     
     private String nombre;
     public String getNombre(){return nombre;}
@@ -13,13 +13,30 @@ public class Cliente {
     public String getApellidos(){return apellidos;}
     public void setApellidos(String valor){apellidos=valor;}
     
-    private String teléfono;
-    public String getTeléfono(){return teléfono;}
-    public void setTeléfono(String valor){teléfono=valor;}
+    private String telefono;
+    public String getTelefono(){return telefono;}
+    public void setTelefono(String valor){telefono=valor;}
     
     private String email;
     public String getEmail(){return email;}
     public void setEmail(String valor){email=valor;}
     
+    public Cliente(){
+    }
+
+    public Cliente(String nombre,String apellidos,String telefono,String email){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;   
+    }
     
+    @Override
+    public String toString(){
+        return nombre+" | "+apellidos+" | "+telefono+" | "+email;
+    }
+    
+    public String toCSV(){
+        return nombre+","+apellidos+","+telefono+","+email;
+    }
 }
