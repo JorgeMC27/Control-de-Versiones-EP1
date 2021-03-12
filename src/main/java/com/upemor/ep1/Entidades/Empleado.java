1944801878014af9d73dea9b0b08415813a737a6
hstol <hstol@LAPTOP-HBRRB4RJ>
2021-03-11 21:56:28 -0600

    Agregue los atributos de Empleado

--- src/main/java/com/upemor/ep1/Entidades/Empleado.java
+++ src/main/java/com/upemor/ep1/Entidades/Empleado.java
@@ -1,4 +1,3 @@
-
 package com.upemor.ep1.Entidades;
 
 /**
@@ -6,4 +5,27 @@ package com.upemor.ep1.Entidades;
  **/
 public class Empleado {
     
+    private String nombre;
+    public String getNombre(){return nombre;}
+    public void setNombre(String valor){nombre=valor;}
+    
+    private String apellidos;
+    public String getApellidos(){return apellidos;}
+    public void setApellidos(String valor){apellidos=valor;}
+    
+    private String departamento;
+    public String getDepartamento(){return departamento;}
+    public void setDepartamento(String valor){departamento=valor;}
+    
+    private String cargo;
+    public String getCargo(){return cargo;}
+    public void setCargo(String valor){cargo=valor;}
+    
+    private String teléfono;
+    public String getTeléfono(){return teléfono;}
+    public void setTeléfono(String valor){teléfono=valor;}
+    
+    private String email;
+    public String getEmail(){return email;}
+    public void setEmail(String valor){email=valor;}
 }
