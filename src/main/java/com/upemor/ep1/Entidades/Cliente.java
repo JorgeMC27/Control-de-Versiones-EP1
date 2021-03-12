754aa902608319ba7654657f3beb62a1157ce56b
hstol <hstol@LAPTOP-HBRRB4RJ>
2021-03-11 21:49:31 -0600

    Agregue los atributos de cliente

--- src/main/java/com/upemor/ep1/Entidades/Cliente.java
+++ src/main/java/com/upemor/ep1/Entidades/Cliente.java
@@ -1,9 +1,25 @@
-
 package com.upemor.ep1.Entidades;
 
 /**
  @author JORGE
  **/
 public class Cliente {
+    
+    private String nombre;
+    public String getNombre(){return nombre;}
+    public void setNombre(String valor){nombre=valor;}
+    
+    private String apellidos;
+    public String getApellidos(){return apellidos;}
+    public void setApellidos(String valor){apellidos=valor;}
+    
+    private String teléfono;
+    public String getTeléfono(){return teléfono;}
+    public void setTeléfono(String valor){teléfono=valor;}
+    
+    private String email;
+    public String getEmail(){return email;}
+    public void setEmail(String valor){email=valor;}
+    
     
 }
